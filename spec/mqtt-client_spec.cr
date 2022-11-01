744d8bfba32d5024d3f2de5f6a37f619673056b3
Jon Börjesson <jon@spuun.se>
2022-11-01 15:29:34 +0100

    Refactor specs to not rely on any mqtt server
    "Mock" server with a raw TCPServer and use mqtt-protocol for parsing
    mqtt protocol.

--- spec/mqtt-client_spec.cr
+++ spec/mqtt-client_spec.cr
@@ -1,36 +1,103 @@
 require "./spec_helper"
 
+alias MP = MQTT::Protocol
+
 describe MQTT::Client do
   it "can publish" do
-    done = Channel(Nil).new
-    mqtt = MQTT::Client.new(MQTT_HOST, 1883, client_id: "can publish")
-    mqtt.on_message do |msg|
-      msg.topic.should eq "foo"
-      msg.body.should eq "bar".to_slice
-      msg.ack
-      done.send nil
+    with_server_socket do |server|
+      done = Channel(Nil).new(1)
+
+      # This "mocks" the server
+      server.with_client do |client_io|
+        MP::Packet.from_io(client_io)
+        MP::Connack.new(false, MP::Connack::ReturnCode::Accepted).to_io(client_io)
+
+        sub = MP::Packet.from_io(client_io).as(MP::Subscribe)
+        MP::SubAck.new([MP::SubAck::ReturnCode::QoS1], sub.packet_id).to_io(client_io)
+
+        subscribed = true
+        loop do
+          packet = MP::Packet.from_io(client_io)
+          case packet
+          when MP::Publish
+            pub = packet.as(MP::Publish)
+            MP::PubAck.new(pub.packet_id.not_nil!).to_io(client_io)
+            pub.to_io(client_io) if subscribed
+          when MP::Unsubscribe
+            unsub = packet.as(MP::Unsubscribe)
+            subscribed = false
+            MP::UnsubAck.new(unsub.packet_id).to_io(client_io)
+          when MP::Disconnect
+            break
+          end
+        end
+        done.send(nil)
+      end
+      mqtt = MQTT::Client.new(server.address.address, server.address.port, client_id: "can publish")
+
+      recieved_message_count = 0
+      mqtt.on_message do |msg|
+        msg.topic.should eq "foo"
+        msg.body.should eq "bar".to_slice
+        msg.ack
+        recieved_message_count += 1
+      end
+      mqtt.subscribe("foo", 1)
+      mqtt.publish("foo", "bar", 1)
+      mqtt.publish("foo", "bar", 1)
+      mqtt.unsubscribe("foo")
+      mqtt.publish("foo", "bar", 1)
+      mqtt.disconnect
+      mqtt.close
+      done.receive
+      recieved_message_count.should eq 2
+      #    ensure
+      #      mqtt.try &.close
     end
-    mqtt.subscribe("foo", 1)
-    mqtt.publish("foo", "bar", 1)
-    mqtt.publish("foo", "bar", 1)
-    mqtt.unsubscribe("foo")
-    mqtt.publish("foo", "bar", 1)
-    done.receive
-    done.receive
-  ensure
-    mqtt.try &.close
   end
 
   it "can ping" do
-    mqtt = MQTT::Client.new(MQTT_HOST, 1883, client_id: "can ping")
-    mqtt.ping
-    mqtt.@connection.@last_packet_received.should be_close Time.monotonic, 1.second
+    with_server_socket do |server|
+      done = Channel(Nil).new(1)
+
+      server.with_client do |client_io|
+        MP::Packet.from_io(client_io)
+        MP::Connack.new(false, MP::Connack::ReturnCode::Accepted).to_io(client_io)
+
+        MP::Packet.from_io(client_io).as(MP::PingReq)
+        MP::PingResp.new.to_io(client_io)
+
+        done.send(nil)
+      end
+
+      mqtt = MQTT::Client.new(server.address.address, server.address.port, client_id: "can ping")
+      mqtt.ping
+      done.receive
+      mqtt.@connection.@last_packet_received.should be_close Time.monotonic, 1.second
+    end
   end
 
   it "can keepalive" do
-    mqtt = MQTT::Client.new(MQTT_HOST, 1883, keepalive: 1u16, client_id: "can keepalive")
-    sleep 1.5
-  ensure
-    mqtt.try &.close
+    with_server_socket do |server|
+      done = Channel(Nil).new(1)
+      ping_recieved = false
+
+      server.with_client do |client_io|
+        connect = MP::Packet.from_io(client_io).as(MP::Connect)
+        client_io.@io.as(Socket).read_timeout = connect.keepalive * 1.5
+        MP::Connack.new(false, MP::Connack::ReturnCode::Accepted).to_io(client_io)
+
+        MP::Packet.from_io(client_io).as(MP::PingReq)
+        ping_recieved = true
+      rescue IO::Error
+        # nop
+      ensure
+        done.send(nil)
+      end
+
+      MQTT::Client.new(server.address.address, server.address.port, keepalive: 1u16, client_id: "can keepalive")
+      done.receive
+      ping_recieved.should be_true
+    end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,27 @@
 require "spec"
 require "../src/mqtt-client"
+require "mqtt-protocol"
 
-MQTT_HOST = ENV.fetch("MQTT_HOST", "localhost")
+class SpecServer
+  def initialize(@server : TCPServer)
+  end
+
+  def address
+    @server.local_address
+  end
+
+  def with_client(&blk : MQTT::Protocol::IO -> Nil)
+    spawn(name: "with_client") do
+      client_socket = @server.accept?
+      raise "accept? returned nil" if client_socket.nil?
+      blk.call MQTT::Protocol::IO.new(client_socket)
+      client_socket.close
+    end
+  end
+end
+
+def with_server_socket
+  TCPServer.open("127.0.0.1", 0, reuse_port: false) do |tcp|
+    yield SpecServer.new(tcp)
+  end
+end
